232c39a7c7ff432d6786e5c0498fe046d83e9edc
RaffiArganta99 <e41231765@student.polije.ac.id>
2025-04-26 22:32:15 +0700

    auto login

--- easy_park/lib/SplashScreen.dart
+++ easy_park/lib/SplashScreen.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'views/user/login_screen.dart';  // Path diperbarui ke lokasi baru
-
-
+import 'views/user/login_screen.dart';
+import 'services/auth_service.dart';
+import 'widgets/Bottom_Navigation.dart';
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -15,14 +15,29 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
+    checkLoginStatus();
+  }
 
-    // Navigasi otomatis setelah 3 detik
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const LoginScreen()),
-      );
-    });
+  Future<void> checkLoginStatus() async {
+    final result = await AuthService.autoLogin(); // ⬅️ Panggil autoLogin()
+
+    await Future.delayed(const Duration(seconds: 2)); // 🔵 kasih delay biar splash muncul 2 detik
+
+    if (mounted) {
+      if (result['success'] == true) {
+        // Kalau autoLogin berhasil, ke HomeScreen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const BottomNavigationWidget()),
+        );
+      } else {
+        // Kalau gagal, ke LoginScreen
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginScreen()),
+        );
+      }
+    }
   }
 
   @override
