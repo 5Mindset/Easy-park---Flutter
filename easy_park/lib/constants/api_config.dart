523a377e0dfc29fc42a8b826ef3bc10fa6fdf15e
RaffiArganta99 <e41231765@student.polije.ac.id>
2025-05-24 22:35:03 +0700

    update: name + icon

--- easy_park/lib/constants/api_config.dart
+++ easy_park/lib/constants/api_config.dart
@@ -1,2 +1,2 @@
-const String apiBaseUrl = 'http://192.168.1.3:8000/api'; // API base URL
-const String baseUrl = 'http://192.168.1.3:8000'; // Base URL for images and other assetsm
+const String apiBaseUrl = 'http://192.168.1.17:8000/api'; // API base URL
+const String baseUrl = 'http://192.168.1.17:8000'; // Base URL for images and other assetsm
